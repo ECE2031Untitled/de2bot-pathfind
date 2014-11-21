b95b5439cee72dcf32055589c1cb800fb7d898e2
StephenAHughes <stevennhughess@gmail.com>
2014-11-20 20:32:47 -0500

    Update Nav.asm
    some tile conversions added... not functional yet

--- asm/Nav.asm
+++ asm/Nav.asm
@@ -148,3 +148,16 @@ ORIENT: DW 0
 ;; Desired direction. One of (0,1,2,3).
 ;;------------------------------------------------------------------------------
 P: DW 0
+
+;;---int TilesToCounts----------------------------------------------------------
+;;  Is set to tiles we desire to travel. Is converted to counts(1.04mm/count) and then assigned to StepX or StepY
+;;-------------------------------------------------------------------------------
+
+TILESTOCOUNTS: DW 0
+
+;CONSTANTS: TILES CONVERTED TO UNITS WE NEEDTOGO
+ONET:586
+TWOT:1172
+THREET:1758
+FOURT:2344
+FIVET:2390
